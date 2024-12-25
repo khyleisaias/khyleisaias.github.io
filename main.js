5518abea9c40c99824224a1f9002a049bf96fd73
kernelk14 <khyleisaias@gmail.com>
2024-12-25 09:20:49 +0800

    Added new site because it's christmas

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-function print(x) {
-    document.getElementById("p").innerText = x
+const date = new Date()
+
+var mm = date.getMonth() + 1
+var dd = date.getDate()
+var yy = date.getFullYear()
+
+//var dd = 1
+
+
+
+function isChristmas(mm, dd, yy) {
+    if (mm === 12) {
+        if (dd < 25) {
+            document.getElementById("header1").innerText = `Date today is ${mm}-${dd}-${yy}`
+            document.getElementById("par1").innerHTML = `Not yet Christmas<br><br>Still <mark>${25 - dd} day/s</mark> before Christmas`     
+        } else if (dd > 25) {
+            document.getElementById("header1").innerText = `Date today is ${mm}-${dd}-${yy}`
+            document.getElementById("par1").innerText = "Christmas has passed"
+        } else {
+            document.getElementById("header1").innerText = `Date today is ${mm}-${dd}-${yy}`
+            document.getElementById("par1").innerText = "It's Christmas!!\nMerry Christmas to you!!"
+        }
+    } else {
+        document.getElementById("header1").innerText = `Date today is ${mm}-${dd}-${yy}` 
+        document.getElementById("par1").innerText = "Not yet Christmas"
+    }
+    
 }
 
-while (document.activeElement) {
-    console.log(document.activeElement)
-}
-function letssee() {
-    var see = document.getElementById("infobox").value
-    print(see)
-}
+isChristmas(mm, dd, yy)
